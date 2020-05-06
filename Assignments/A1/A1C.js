2257bf498a2df1ad894db42e12751970bfda8423
Ian Page <ian.am.page@gmail.com>
2020-05-06 14:52:29 -0300

    refactored A1C to specification

--- Assignments/A1/A1C.js
+++ Assignments/A1/A1C.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
-app.use(express.json());
+const express023 = require('express');
+const mysql023 = require('mysql');
+const app023 = express023();
+app023.use(express023.json());
 
-const db = mysql.createConnection({
+const db023 = mysql023.createConnection({
   host: '127.0.0.1',
   user: 'cunt',
   password: 'password',
@@ -11,64 +11,63 @@ const db = mysql.createConnection({
   database: 'A1C'
 });
 
-db.connect((err) => {
-  if(err) {
-    throw err;
+db023.connect((err023) => {
+  if(err023) {
+    throw err023;
   }
   console.log('Mysql connected');
 });
 
-app.get('/', (req, res) => {
-  let sql = 'SELECT * FROM new_table';
-  let query = db.query(sql, (err, records) => {
-    if(err) {
-      res.statusCode = 400;
-      res.send('Error: Could not retrieve record info');
+app023.get('/', (req023, res023) => {
+  let sql023 = 'SELECT * FROM new_table';
+  let query023 = db023.query(sql023, (err023, records023) => {
+    if(err023) {
+      res023.statusCode = 400;
+      res023.send('Error: Could not retrieve record info');
     }
-    res.send(JSON.stringify(records));
+    res023.send(JSON.stringify(records023));
   });
 });
 
-app.get('/:jobName/:partId', (req, res) => {
-  var sql = "SELECT * FROM new_table WHERE job_name = '" + req.params.jobName + "' AND part_id = " + req.params.partId + " LIMIT 1";
-  let query = db.query(sql, (err, record) => {
-    if(err || record.length === 0) {
-      res.statusCode = 400;
-      res.send('Error: Could not retrieve qty info');
+app023.get('/:jobName023/:partId023', (req023, res023) => {
+  var sql023 = "SELECT * FROM new_table WHERE job_name = '" + req023.params.jobName023 + "' AND part_id = " + req023.params.partId023 + " LIMIT 1";
+  let query023 = db023.query(sql023, (err023, record023) => {
+    if(err023 || record023.length === 0) {
+      res023.statusCode = 400;
+      res023.send('Error: Could not retrieve qty info');
     } else {
-      res.send(JSON.stringify(record));
+      res023.send(JSON.stringify(record023));
     }  
   });
 });
 
-app.post('/', (req, res) => {
+app023.post('/', (req023, res023) => {
 
-  var sql = "INSERT INTO new_table VALUES('" + req.body.jobName + "', " + req.body.partId + ", " + req.body.qty + ")";
-  let query = db.query(sql, (err, result) => {
-    if(err) {
-      res.statusCode = 400;
-      res.send("ERROR: " + err.sqlMessage);
+  var sql023 = "INSERT INTO new_table VALUES('" + req023.body.jobName023 + "', " + req023.body.partId023 + ", " + req023.body.qty023 + ")";
+  let query023 = db023.query(sql023, (err023, result023) => {
+    if(err023) {
+      res023.statusCode = 400;
+      res023.send("ERROR: " + err023.sqlMessage);
     } else {
-      res.send("Success");
+      res023.send("Success");
     }
   });
-
 });
 
-app.put('/', (req, res) => {
+app023.put('/', (req023, res023) => {
 
-  var sql = "UPDATE new_table SET quantity = " + req.body.qty + " WHERE job_name = '" + req.body.jobName + "' AND part_id = " + req.body.partId; 
-  let query = db.query(sql, (err, result) => {
+  var sql023 = "UPDATE new_table SET quantity = " + req023.body.qty023 + " WHERE job_name = '" + req023.body.jobName023 + "' AND part_id = " + req023.body.partId023; 
+  let query = db023.query(sql023, (err, result) => {
     if(err) {
-      res.statusCode = 400;
-      res.send("ERROR: " + err.sqlMessage);
+      res023.statusCode = 400;
+      res023.send("ERROR: " + err.sqlMessage);
     }else if(result.affectedRows === 0) {
-      res.statusCode = 400;
-      res.send("ERROR: record not found");
+      res023.statusCode = 400;
+      res023.send("ERROR: record not found");
     } else {
-      res.send("Success");
+      res023.send("Success");
     }
   });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app023.listen(3000, () => console.log('Listening on port 3000...'));
