5d2e9984527145ee802382758772ecd1d388dcfb
Ian Page <ian.am.page@gmail.com>
2020-06-16 19:01:03 -0300

    got app working locally

--- Assignments/A5/api/controllers/LabController.js
+++ Assignments/A5/api/controllers/LabController.js
@@ -1,84 +1,99 @@
-let parts = [
-    {id:1, name:"Part1"},
-    {id:2, name:"Part2"},
-    {id:3, name:"Part3"}
-  ]
+let data023 = [
+    {jobName023: "Job1023", partId023: 1023, qty023: 55},
+    {jobName023: "Job2023", partId023: 2023, qty023: 100},
+    {jobName023: "Job3023", partId023: 3023, qty023: 200}
+]
   
-  module.exports = {
-    getParts:function(req, res) {
-      res.send(parts)
+module.exports = {
+
+    getAll023:function(req, res) {
+        res.send(data023)
     },
     
-    getPartsByID : function(req, res) {
-      var flag = 0;
-      for(var i = 0; i < parts.length; i++) {
+    getRecordByIds023 : function(req, res) {
+        var flag023 = 0;
+        for(var i = 0; i < data023.length; i++) {
   
-        if(parts[i].id == parseInt(req.params.id)) {
-          flag = 1;
-          varobj = parts[i];
+            if(data023[i].jobName023 == req.params.jobName023 
+            && data023[i].partId023 == parseInt(req.params.partId023)) {
+          
+                flag023 = 1;
+                var obj023 = data023[i];
+            }
         }
-      }
   
-      if(flag == 1) {
-        res.send(obj);
-      }
-  
-      if(flag == 0){
-        res.send({
-          code:"404",
-          message:"ID Not Found"
-        })
-      }
-    },
-    
-    addParts : function(req, res) {
-      var flag = 0;
-      for(var i = 0; i < parts.length; i++) {
-        if(parts[i].id == parseInt(req.body.id)) {
-          flag= 1;
+        if(flag023 == 1) {
+            res.send(obj023);
         }
-      }
   
-      if(flag == 1) {
-        res.send({
-          code:"400",
-          message:"ID already exist"
-        })
-      }
-  
-      if(flag == 0) {
-        var part = {id:parseInt(req.body.id), name:req.body.name}
-        parts.push(part)
-        res.send(parts[parts.length-1])
-      }
-    },
-    
-    viewData: function(req, res) {
-      if(!parts) {
-        res.send("Cannot find anything to show!")
-      }
-      if(parts) {
-        res.view("pages/viewData", {parts:parts})
-      }
-    },
-    
-    addData: function(req, res) {
-      var flag = 0;
-      for(var i = 0; i<parts.length; i++) {
-        if(parts[i].id == parseInt(req.body.id)) {
-          flag = 1;
+        if(flag023 == 0){
+            res.send({
+                code:"404",
+                message:"Record Not Found"
+            })
         }
-      }
-      if(flag == 1) {
-        res.send({
-          code:"400",
-          message:"ID already exist"
-        })
-      }
-      if(flag == 0) {
-        var part= {id:parseInt(req.body.id), name:req.body.name}
-        parts.push(part)
-        res.redirect("/viewData")
-      }
+    },
+    
+    addRecord023 : function(req, res) {
+        var flag = 0;
+        for(var i = 0; i < data023.length; i++) {
+            if(data023[i].jobName023 == req.params.jobName023 
+            && data023[i].partId023 == parseInt(req.params.partId023)) {
+                flag= 1;
+            }
+        }
+  
+        if(flag == 1) {
+            res.send({
+                code:"400",
+                message:"ID already exist"
+            })
+        }
+  
+        if(flag == 0) {
+            var record023 = {
+                jobName023:req.body.jobName023, 
+                partId023: parseInt(req.body.partId023),
+                qty023: parseInt(req.body.qty023)
+            }
+            data023.push(record023)
+            res.send(data023[data023.length-1])
+        }
+    },
+    
+    viewData023: function(req, res) {
+        if(!data023) {
+            res.send("Cannot find anything to show!")
+        }
+        if(data023) {
+            res.view("pages/viewData", {data023:data023})
+        }
+    },
+    
+    addData023: function(req, res) {
+        var flag = 0;
+        for(var i = 0; i < data023.length; i++) {
+            if(data023[i].jobName023 == req.params.jobName023 
+            && data023[i].partId023 == parseInt(req.params.partId023)) {
+                flag= 1;
+            }
+        }
+    
+        if(flag == 1) {
+            res.send({
+                code:"400",
+                message:"ID already exist"
+            })
+        }
+    
+        if(flag == 0) {
+            var record023 = {
+                jobName023:req.body.jobName023, 
+                partId023: parseInt(req.body.partId023),
+                qty023: parseInt(req.body.qty023)
+            }
+            data023.push(record023)
+            res.redirect("/viewData023")
+        }
     }
-  }
+}
--- Assignments/A5/config/routes.js
+++ Assignments/A5/config/routes.js
@@ -1,10 +1,10 @@
 
 module.exports.routes = {
-  '/':                      { view: 'pages/homepage' },
-  'GET /getParts':          'LabController.getParts',
-  'GET /getPartsByID/:id':  'LabController.getPartsByID',
-  'POST /addParts':         'LabController.addParts',
-  'GET /viewData':          'LabController.viewData',
-  'GET /addData':           { view: 'pages/addData'},
-  'POST /addData':          'LabController.addData'
+  '/':                                            { view: 'pages/homepage' },
+  'GET /getAll023':                             'LabController.getAll023',
+  'GET /getRecordByIds023/:JobId023/PartId023':   'LabController.getRecordByIds023',
+  'POST /addRecord023':                               'LabController.addRecord023',
+  'GET /viewData023':                                'LabController.viewData023',
+  'GET /addData023':                                 { view: 'pages/addData'},
+  'POST /addData023':                                'LabController.addData023'
 };
